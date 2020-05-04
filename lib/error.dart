1002eab66996f2d18b5a93050d3e5800decebce2
Ashish Rao <ashishrao2598@gmail.com>
2020-05-05 01:06:56 +0530

    initial commit

---
+++ lib/error.dart
@@ -0,0 +1,85 @@
+import 'package:alfredexpensetracker/main.dart';
+import 'package:flutter/material.dart';
+
+class ErrorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DisplayErrorPage(),
+    );
+  }
+}
+
+class DisplayErrorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Oops! Looks like we',
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 40.0,
+                  ),
+                ),
+                Text(
+                  'Messed Up!',
+                  style: TextStyle(
+                    color: Colors.redAccent,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                Image(
+                    image: AssetImage('images/bermuda-page-not-found.png'),
+                    height: 250.0),
+                SizedBox(
+                  height: 25.0,
+                ),
+                Text(
+                  'Try again later',
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 40.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                MaterialButton(
+                  color: Colors.pinkAccent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  padding: EdgeInsets.only(left: 50.0, right: 50.0),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MyApp(),
+                        ));
+                  },
+                  child: Text(
+                    'BACK HOME',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
