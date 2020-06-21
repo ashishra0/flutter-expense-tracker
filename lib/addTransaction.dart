78120f3964e3d02d6c4a5843175eb42ce371c5f3
Ashish Rao <ashishrao2598@gmail.com>
2020-06-22 01:03:02 +0530

    remove unused package

--- lib/addTransaction.dart
+++ lib/addTransaction.dart
@@ -1,4 +1,3 @@
-import 'package:alfredexpensetracker/databasehelper.dart';
 import 'package:alfredexpensetracker/item.dart';
 import 'package:flutter/material.dart';
 import 'package:progress_dialog/progress_dialog.dart';
